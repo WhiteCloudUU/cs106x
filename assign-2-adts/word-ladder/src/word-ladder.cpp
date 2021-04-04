d7b1b9decfbdc7fb339162a133d3d4fa173191e4
WhiteCloudUU <jinzhao.chang.bayarea@gmail.com>
2021-04-04 10:24:08 -0700

    Add assignment 2 word ladder

--- assign-2-adts/word-ladder/src/word-ladder.cpp
+++ assign-2-adts/word-ladder/src/word-ladder.cpp
@@ -12,6 +12,10 @@ using namespace std;
 #include "strlib.h"
 #include "simpio.h"
 
+#include "vector.h"
+#include "queue.h"
+#include "set.h"
+
 static string getWord(const Lexicon& english, const string& prompt) {
     while (true) {
         string response = trim(toLowerCase(getLine(prompt)));
@@ -20,9 +24,66 @@ static string getWord(const Lexicon& english, const string& prompt) {
     }
 }
 
+
+
 static void generateLadder(const Lexicon& english, const string& start, const string& end) {
     cout << "Here's where you'll search for a word ladder connecting \"" << start << "\" to \"" << end << "\"." << endl;
+
+    Vector<string> word_ladder;
+    word_ladder.add(start);
+    Set<string> words_in_use;
+    words_in_use.add(start);
+    Queue<Vector<string>> queue;
+    queue.add(word_ladder);
+
+    string top_word;
+    while (!queue.isEmpty()) {
+        word_ladder = queue.dequeue();
+        top_word = word_ladder[word_ladder.size() - 1];
+        if (top_word == end) {
+            break;
+        }
+
+        for (int i = 0; i < top_word.length(); i++) {
+            for (int j = 0; j < 26; j++) {
+                if (j != top_word[i] - 'a') {
+                    string top_word_copy = top_word;
+                    top_word_copy[i] = 'a' + j;
+                    if (english.contains(top_word_copy) && !words_in_use.contains(top_word_copy)) {
+                        words_in_use.add(top_word_copy);
+                        Vector<string> word_ladder_copy = word_ladder;
+                        word_ladder_copy.add(top_word_copy);
+                        queue.add(word_ladder_copy);
+                    }
+                }
+            }
+        }
+
+    }
+
+    cout << "Found ladder: ";
+    for (int i = 0; i < word_ladder.size() - 1; i++) {
+        cout << word_ladder[i] << " --> ";
+    }
+    cout << word_ladder[word_ladder.size() - 1] << endl;
+
 }
+
+/*
+ * Function: find_one_hop_words
+ * ----------------------------
+ * for each word that differs by one char from top word
+ * and exists in the lexicon
+ * and has not already been used in some other ladder,
+ * 1. create copy of partial ladder,
+ * 2. extend this ladder by pushing new word on top,
+ * 3. enqueue this ladder at end of queue
+ */
+void find_one_hop_words(string word, Vector<string> word_ladder) {
+    return;
+}
+
+
 
 static const string kEnglishLanguageDatafile = "dictionary.txt";
 static void playWordLadder() {
